c1952a85a00349bb1766b898f65603581851bfc3
Giel <gbussel58@gmail.com>
2019-05-21 11:22:45 +0200

    Basis

---
+++ Restaurant/Form1.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// This is the code for your desktop app.
+// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
+namespace Restaurant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+  
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Waiter main = new Waiter();
+            main.Show();
+            this.Hide();
+        }
+
+        //Zorgt ervoor dat het kruisje rechtbovenin daadwerkelijk de app afsluit
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Restaurant/order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant
+{
+    public partial class order : Form
+    {
+        public order()
+        {
+            InitializeComponent();
+        }
+
+        private void order_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void order_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Terug_Click(object sender, EventArgs e)
+        {
+            Waiter main = new Waiter();
+            main.Show();
+            this.Hide();
+        }
+    }
+}
